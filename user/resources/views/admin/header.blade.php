9806915b610d01e0a4dc5ebf8c598aa02a012fcd
X-nimesh <nimush14@gmail.com>
2022-04-20 12:04:00 +0545

    halka change

--- user/resources/views/admin/header.blade.php
+++ user/resources/views/admin/header.blade.php
@@ -394,20 +394,20 @@
            
           </div>
         </div>
-            <script src="./libs/jquery/jquery.min.js"></script>
-    <script src="./libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="./libs/metismenu/metisMenu.min.js"></script>
-    <script src="./libs/simplebar/simplebar.min.js"></script>
+            <script src="{{asset ('./libs/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset ('./libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset ('./libs/metismenu/metisMenu.min.js')}}"></script>
+    <script src="{{asset ('./libs/simplebar/simplebar.min.js')}}"></script>
     <script src="./libs/node-waves/waves.min.js"></script>
 
     <!-- apexcharts -->
-    <script src="./libs/apexcharts/apexcharts.min.js"></script>
+    <script src="{{asset ('./libs/apexcharts/apexcharts.min.js')}}"></script>
 
     <!-- Plugins js-->
-    <script src="./libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
-    <script src="./libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="{{asset ('./libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+    <script src="{{asset ('./libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js')}}"></script>
 
-    <script src="./js/pages/dashboard.init.js"></script>
+    <script src="{{asset ('./js/pages/dashboard.init.js')}}"></script>
 
-    <script src="./js/app.js"></script>
+    <script src="{{asset ('./js/app.js')}}"></script>
       </header>
