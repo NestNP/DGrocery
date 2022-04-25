7dd702bac59c20e156955d6ffb2e9ff7325a6bdb
bishanynwa <96219530+bishanynwa@users.noreply.github.com>
2022-04-25 11:58:50 +0545

    gitcheck

--- user/database/migrations/2022_04_20_072825_products.php
+++ user/database/migrations/2022_04_20_072825_products.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Products extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -24,8 +24,6 @@ class CreateUsersTable extends Migration
         });
     }
 
-
-
     /**
      * Reverse the migrations.
      *
@@ -33,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
