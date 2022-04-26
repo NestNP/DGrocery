b7bc5679c03a7bf9e4dbfa3fcede820d29f7dcaf
bishanynwa <96219530+bishanynwa@users.noreply.github.com>
2022-04-26 11:28:37 +0545

    Bishanordersamma

--- user/resources/views/admin/script.blade.php
+++ user/resources/views/admin/script.blade.php
@@ -55,5 +55,35 @@
  
      <script src="{{asset ('assetsAdmin/js/app.js')}}"></script>
 
+{{-- .............................. --}}
+<script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+     <script src="{{asset('assetsAdmin/libs/metismenu/metisMenu.min.js')}}"></script>
+     <script src="{{asset('assetsAdmin/libs/simplebar/simplebar.min.js')}}"></script>
+     <script src="{{asset('assetsAdmin/libs/node-waves/waves.min.js')}}"></script>
 
- 
+     <!-- Bootrstrap touchspin -->
+     <script src="{{asset('assetsAdmin/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js')}}"></script>
+
+     <script src="{{asset('assetsAdmin/js/pages/ecommerce-cart.init.js')}}"></script>
+
+     <script src="{{asset('assetsAdmin/js/app.js')}}"></script>
+
+     {{-- .................Cart...................... --}}
+     <script src="{{asset('assetsAdmin/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('assetsAdmin/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('assetsAdmin/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{asset('assetsAdmin/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('assetsAdmin/libs/node-waves/waves.min.js')}}"></script>
+
+        <!-- Required datatable js -->
+        <script src="{{asset('assetsAdmin/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('assetsAdmin/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+        
+        <!-- Responsive examples -->
+        <script src="{{asset('assetsAdmin/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{asset('assetsAdmin/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
+
+        <script src="{{asset('assetsAdmin/js/pages/ecommerce-datatables.init.js')}}"></script>
+
+        <script src="{{asset('assetsAdmin/js/app.js')}}"></script>
