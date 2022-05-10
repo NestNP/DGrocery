eb785b55aa07da3a6b285bb56a5646295e7599db
bishanynwa <96219530+bishanynwa@users.noreply.github.com>
2022-05-10 11:42:45 +0545

    insertcategory

--- user/app/Http/Controllers/admin/AdminProducts.php
+++ user/app/Http/Controllers/admin/AdminProducts.php
@@ -21,6 +21,25 @@ class AdminProducts extends Controller
     {
         return view('/admin/addproduct');
     }
+    public function insertproduct(Request $request){
+        $Product_name = $request->input('Product_name');
+        $Manufacturer_name = $request->input('Manufacturer_name');
+        $Manufacturer_Brand = $request->input('Manufacturer_Brand');
+        $Price = $request->input('Price');
+        $Category = $request->input('Category');
+        $Features = $request->input('Features');
+        $Product_description = $request->input('Product_description');
+        $Product_image = $request->input('Product_image');
+        $quantity = $request->input('quantity');
+        $Meta_data = $request->input('Meta_data');
+        $Meta_title = $request->input('Meta_title');
+        $Meta_keywords = $request->input('Meta_keywords');
+        $Meta_description = $request->input('Meta_description');
+        $data=array('Product_name'=>$Product_name,"Manufacturer_name"=>$Manufacturer_name,"Manufacturer_Brand"=>$Manufacturer_Brand,"Price"=>$Price,"Category"=>$Category,"Features"=>$Features,"Product_description"=>$Product_description,"Product_image"=>$Product_image,"quantity"=>$quantity,"Meta_data"=>$Meta_data,"Meta_title"=>$Meta_title,"Meta_keywords"=>$Meta_keywords,"Meta_description"=>$Meta_description);
+        DB::table('products')->insert($data);
+        // echo "Record inserted successfully.<br/>";
+        // echo '<a href = "/insert">Click Here</a> to go back.';
+        }
    
 
 }
--- user/database/migrations/2022_05_04_084938_category.php
+++ user/database/migrations/2022_05_04_084938_category.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// namespace App\Http\Controllers\admin\DB; 
+// use Illuminate\Support\Facades\DB;
+
 class Category extends Migration
 {
     /**
